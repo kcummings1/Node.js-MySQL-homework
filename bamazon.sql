2fabbbd3d7be82dab8baf7e13ab7ae9c5a8696ff
Kobi Cummings <kobicummings@gmail.com>
2019-09-27 18:13:19 -0700

    started making changes on js and sql

--- bamazon.sql
+++ bamazon.sql
@@ -17,23 +17,23 @@ CREATE TABLE products (
 
 SELECT * FROM products;
 
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Code Names", "Games", 19.95, 15);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Taboo", "Games", 10.95, 10);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Scattergories", "Games", 20.49, 2);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Catan", "Games", 24.95, 5);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("The Girl on the Train", "Books", 14.95, 5);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Behind Closed Doors", "Books", 10.99, 1);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Harry Potter and the Cursed Child", "Books", 14.95, 10);
-INSERT INTO products ( ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("MacBook Air", "Computers", 899.99, 3);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("HP", "Computers", 479.99, 10);
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Dell", "Computers", 499.99, 1);
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (Product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
+INSERT INTO products (Product_name,department_name,price,stock_quantity)
+VALUES ("", "", , );
