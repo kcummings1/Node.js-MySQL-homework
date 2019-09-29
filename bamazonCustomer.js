25bfe6eda9849168f620971b0b973451cd71bc8c
Kobi Cummings <kobicummings@gmail.com>
2019-09-29 11:11:06 -0700

    added text to js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,18 @@
 var mysql = require ("mysql");
 var inquirer = require ("inquirer");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+port: 3306,
+
+//----username------
+
+user: "root",
+
+//------my pw-------
+
+password: "password",
+database: "bamazon_db"
+});
+
