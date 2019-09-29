8e84c13d6a041058e4fa41c0aeb7c49694563641
Kobi Cummings <kobicummings@gmail.com>
2019-09-29 12:02:22 -0700

    changes in package.json

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,49 @@
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
 
-port: 3306,
+    port: 3306,
 
-//----username------
+    //----username------
 
-user: "root",
+    user: "root",
 
-//------my pw-------
+    //------my pw-------
 
-password: "password",
-database: "bamazon_db"
+    password: "password",
+    database: "bamazon_db"
 });
+//--------Product ID ----///----Number of units------
+
+var prompts = {
+    productIDPrompt: {
+        type: "input",
+        name: "products",
+        message: "Please enter the product ID of the product you would like to purchase: "
+    },
+    numberOfUnitsPrompt: {
+        type: "input",
+        name: "numberOfUnits",
+        message: "Please enter the number of units you would like to purchase: "
+    }
+};
+
+// ------Display Products------
+
+var displayProducts = function() {
+    console.log("---------------");
+    console.log("Selecting all products...\n");
+
+    connection.query("SELECT * FROM products", function ( error, result) {
+        if (error) throw error;
+
+        result.forEach(function(element) {
+            console.log("product ID: " + element.itme_id + "\nName: " + element.product_name + "\nPrice($): " + element.price + "\n");
+        });
+        buyProduct();
+    });
+}
+
 
