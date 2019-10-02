ecca928d5e85b2d0ff708515f916db80733d1440
Kobi Cummings <kobicummings@gmail.com>
2019-10-01 18:01:50 -0700

    got it to work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,28 +5,28 @@ var inquirer = require("inquirer");
 //-------------Connect to sql Database----------------
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+	host: "localhost",
+	port: 3306,
 
-    //----Username------
+	//----Username------
 
-    user: "root",
+	user: "root",
 
-    //------My Password-------
+	//------My Password-------
 
-    password: "password",
-    database: "bamazon_db"
+	password: "password",
+	database: "bamazon_db"
 });
 
-connection.connect(function(err){
-	if(err)throw err;
+connection.connect(function (err) {
+	if (err) throw err;
 	console.log("connected as id" + connection.threadId);
 });
 
-var displayProducts = function(){
+var displayProducts = function () {
 	var query = "Select * FROM products";
-	connection.query(query, function(err, res){
-		if(err) throw err;
+	connection.query(query, function (err, res) {
+		if (err) throw err;
 		// var displayTable = new Table ({
 		// 	head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
 		// 	colWidths: [10,25,25,10,14]
@@ -42,43 +42,49 @@ var displayProducts = function(){
 	});
 }
 
-function purchasePrompt(){
-	inquirer.prompt([
-	{
-		name: "ID",
-		type: "input",
-		message:"Please enter Item ID you like to purhcase.",
-		filter:Number
-	},
-	{
-		name:"Quantity",
-		type:"input",
-		message:"How many items do you wish to purchase?",
-		filter:Number
-	},
+function purchasePrompt() {
+	inquirer.prompt([{
+			name: "ID",
+			type: "input",
+			message: "Please enter Item ID you like to purhcase.",
+			filter: Number
+		},
+		{
+			name: "Quantity",
+			type: "input",
+			message: "How many items do you wish to purchase?",
+			filter: Number
+		},
 
- ]).then(function(answers){
- 	var quantityNeeded = answers.Quantity;
- 	var IDrequested = answers.ID;
- 	purchaseOrder(IDrequested, quantityNeeded);
- });
+	]).then(function (answers) {
+		var quantityNeeded = answers.Quantity;
+		var IDrequested = answers.ID;
+		purchaseOrder(IDrequested, quantityNeeded);
+	});
 };
 
-function purchaseOrder(ID, amtNeeded){
-	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
-		if(err){console.log(err)};
-		if(amtNeeded <= res[0].stock_quantity){
+function purchaseOrder(ID, amtNeeded) {
+	connection.query('Select * FROM products WHERE item_id = ' + ID, function (err, res) {
+		if (err) {
+			console.log(err)
+		};
+		if (amtNeeded <= res[0].stock_quantity) {
 			var totalCost = res[0].price * amtNeeded;
 			console.log("Good news your order is in stock!");
-			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+			console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + " is " + totalCost + " Thank you!");
 
-			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
-		} else{
+			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID, function (er, res) {
+				if (err) {
+					console.log(err)
+				};
+				connection.end();
+
+			});
+		} else {
 			console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
 		};
 		//displayProducts();
 	});
 };
 
-displayProducts(); 
-
+displayProducts();
